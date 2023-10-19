f612b33bdafd00fc036ffea7f0423434747afdbe
Кирилл Антонов <antonov.kirill1991@yandex.ru>
2023-10-19 20:28:30 +0300

    final fix

--- backend/users/models.py
+++ backend/users/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class User(AbstractUser):
     """Модель пользователя."""
 
-    first_name = models.CharField(max_length=255, verbose_name="Имя")
-    last_name = models.CharField(max_length=255, verbose_name="Фамилия")
+    first_name = models.CharField(max_length=150, verbose_name="Имя")
+    last_name = models.CharField(max_length=150, verbose_name="Фамилия")
     email = models.EmailField(
         max_length=254, unique=True, verbose_name="Почта"
     )
