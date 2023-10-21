bc3466e8c7ed7d383cef04b01005089e25b2ee7f
Кирилл Антонов <antonov.kirill1991@yandex.ru>
2023-10-21 19:51:39 +0300

    Fix allowed hosts in settings.py_2

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 # cd backend
 # python manage.py migrate
-# python manage.py collectstatic
+python manage.py collectstatic
 cp -r /app/collected_static/. /static/static/
 # python manage.py load_tags_json
 # python manage.py add_ingredients
